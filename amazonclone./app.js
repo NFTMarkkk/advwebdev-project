468c37debc395c8cef8e7c9ff191de89a7bf59d6
Marcoding.io <151931590+NFTMarkkk@users.noreply.github.com>
2024-11-14 17:20:46 +0800

    Add files via upload

---
+++ amazonclone./app.js
@@ -0,0 +1,15 @@
+const leftbtn = document.querySelector(".l-btn");
+const rightbtn = document.querySelector(".r-btn");
+
+rightbtn.addEventListener('click',function(event){
+    console.log('done');
+    const conent = document.querySelector('.product-slide');
+    conent.scrollLeft += 1100;
+    event.preventDefault();
+});
+leftbtn.addEventListener('click',function(event){
+    console.log('done');
+    const conent = document.querySelector('.product-slide');
+    conent.scrollLeft -= 1100;
+    event.preventDefault();
+});
